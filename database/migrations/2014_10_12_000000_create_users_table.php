453311b59102e755ee67688157b432019487240a
MohammedEllouzati2 <mohammedellouzati1@gmail.com>
2024-04-22 18:16:33 +0100

    creation des tables et modules

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom');
+            $table->string('prenom');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role',['Administrateur','Annonceur','Etudiant','Manager','Responsable_Universitaire']);
+            $table->string('Tel',20);
+
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2024_04_22_140617_create_etudiants_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('etudiants', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->enum('level',['Bac','Bac+2','Bac+3','Bac+4','Bac+5']);
+            $table->date('Date_Graduation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('etudiants');
+    }
+};
